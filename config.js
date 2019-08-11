a790a168848dcee6486340b82c86022d14e41134
Zoltaroth <nandor@ntsj.com>
2019-08-11 15:36:35 -0700

    added favicon

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ class homePageConfig
     {
         this.headerColor = "lightgray";          // color of the page heading.
         this.serviceWidth = "100%";              // width of the services inside the center gray box.
-        this.serviceHeight = "45px";             // height of each service.
+        this.serviceHeight = "65px";             // height of each service.
         this.serviceBGColor = "darkslateblue";   // background color of each service.
         this.serviceLinkColor = "white";         // text color of service links.
         this.bgColor = "black";                  // page bg color - only if no image is used.
