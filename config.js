6241c9bfb2a17a3e003efb6dbc411e14dc4d63ec
Zoltaroth <nandor@ntsj.com>
2019-08-12 08:56:52 -0700

    page title / header is now configurable

--- config.js
+++ config.js
@@ -2,13 +2,14 @@ class homePageConfig
 {
     constructor()
     {
-        this.headerColor = "lightgray";          // color of the page heading.
-        this.serviceWidth = "100%";              // width of the services inside the center gray box.
-        this.serviceHeight = "65px";             // height of each service.
-        this.serviceBGColor = "darkslateblue";   // background color of each service.
-        this.serviceLinkColor = "white";         // text color of service links.
-        this.bgColor = "black";                  // page bg color - only if no image is used.
-        this.bgImage = "./dark-texture-bkg.jpg"; // page background image.
+        this.title = "NTSJ Internal Services List"; // page title.
+        this.headerColor = "lightgray";             // color of the page heading.
+        this.serviceWidth = "100%";                 // width of the services inside the center gray box.
+        this.serviceHeight = "65px";                // height of each service.
+        this.serviceBGColor = "darkslateblue";      // background color of each service.
+        this.serviceLinkColor = "white";            // text color of service links.
+        this.bgColor = "black";                     // page bg color - only if no image is used.
+        this.bgImage = "./dark-texture-bkg.jpg";    // page background image.
     }
 }
 
