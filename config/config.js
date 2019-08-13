f6c1f3564a4b568a69a07adc7372748490248c9f
Zoltaroth <nandor@ntsj.com>
2019-08-13 09:02:53 -0700

    added concept of advanced and basic mode

--- config/config.js
+++ config/config.js
@@ -7,7 +7,11 @@ class homePageConfig
         this.serviceWidth = "100%";                        // width of the services inside the center gray box.
         this.serviceHeight = "65px";                       // height of each service.
         this.serviceBGColor = "darkslateblue";             // background color of each service.
+        this.advTextColor = "white";                       // text color for adv button.
+        this.advHighlightColor = "darkslateblue";          // highlight color for adv button.
+        this.advButtonColor = "slateblue";                 // button color for adv button
         this.serviceLinkColor = "white";                   // text color of service links.
+        this.advDefault = true;                            // show advanced by default?
         this.bgColor = "black";                            // page bg color - only if no image is used.
         this.bgImage = "./images/dark-texture-bkg.jpg";    // page background image.
     }
--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,5 @@
+var srvs;
+
 $(document).ready(function()
 {
     // bring in custom configs
@@ -5,20 +7,11 @@ $(document).ready(function()
 
     var table = document.getElementById("services");
 
-    var srvs = JSON.parse(services);
-    var i;
-    for( i = 0; i < srvs.length; i++ )
-    {
-        //console.log(srvs[i].name + " " + srvs[i].url +  " " + srvs[i].icon );
-        var row = table.insertRow(-1);
-        row.classList.add("service-item");
-        var icon = row.insertCell(-1);
-        var link = row.insertCell(-1);
-        icon.classList.add("service-data-img");
-        icon.innerHTML = '<img class="favico" src="' + srvs[i].icon + '">';
-        link.classList.add("service-data-link");
-        link.innerHTML = '<A target="_blank" class="service-link" HREF="' + srvs[i].url + '">' + srvs[i].name + '</A>';
-    }
+    var button = document.getElementById("toggleAdv");
+
+    buildTable( table, button);
+
+    setAdvButton( config, button);
 
     table.style.width = config.serviceWidth;
 
@@ -27,6 +20,12 @@ $(document).ready(function()
     var header = document.getElementById("header");
     header.innerHTML = config.title;
 
+    var allRows = document.getElementsByClassName("button");
+    for (var i = 0; i < allRows.length; i++) 
+    {
+        allRows[i].style.color = config.advTextColor;
+        allRows[i].style.backgroundColor = config.advButtonColor;
+    }
     var allRows = document.getElementsByClassName("head");
     for (var i = 0; i < allRows.length; i++) 
     {
@@ -45,4 +44,80 @@ $(document).ready(function()
     }
     document.body.style.backgroundImage = 'url("' + config.bgImage + '")';
     document.body.style.backgroundColor = config.bgColor;
+
+    addListeners(config);
 });
+
+function setAdvButton( config, button )
+{
+    if( config.advDefault )
+    {
+        toggleAdv( button );
+    }
+}
+
+// 0 = Baisc
+// 1 = Adv 
+function toggleAdv(advButton)
+{
+    if( advButton.value == 1 ) // in adv mode
+    {
+        advButton.value = 0;
+        advButton.innerHTML = "Mode: Basic";
+    }
+    else
+    {
+        advButton.value = 1;
+        advButton.innerHTML = "Mode: Advanced";
+    }
+
+    var table = document.getElementById("services");
+    var i;
+    for( i = 0; i < table.rows.length; i++ )
+    {
+        if( advButton.value == 0 ) // basic mode
+        {
+            table.rows[i].style.visibility = srvs[i].adv ? 'collapse' : 'visible';
+        }
+        else
+        {
+            table.rows[i].style.visibility = 'visible';
+        }
+    }
+}
+
+function buildTable(table, advButton)
+{
+    srvs = JSON.parse(services);
+    var i;
+
+    for( i = 0; i < srvs.length; i++ )
+    {
+        //console.log(srvs[i].name + " " + srvs[i].url +  " " + srvs[i].icon );
+        var row = table.insertRow(-1);
+        row.classList.add("service-item");
+        var icon = row.insertCell(-1);
+        var link = row.insertCell(-1);
+        icon.classList.add("service-data-img");
+        icon.innerHTML = '<img class="favico" src="' + srvs[i].icon + '">';
+        link.classList.add("service-data-link");
+        link.innerHTML = '<A target="_blank" class="service-link" HREF="' + srvs[i].url + '">' + srvs[i].name + '</A>';
+        table.rows[i].style.visibility = srvs[i].adv ? 'collapse' : 'visible';
+    }
+
+}
+
+function addListeners(config)
+{
+    var button = document.getElementById("toggleAdv");
+
+    button.addEventListener('mouseenter', e => {
+        //console.log( "mousein" );
+        button.style.backgroundColor = config.advHighlightColor;
+    });
+
+    button.addEventListener('mouseleave', e => {
+        //console.log( "mouseout" );
+        button.style.backgroundColor = config.advButtonColor;
+    });
+}
