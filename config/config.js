c6ae3607e9c00fb187cd6ad979a979497751ee6a
Zoltaroth <nandor@ntsj.com>
2019-08-12 22:06:51 -0700

    structure cleanup

---
+++ config/config.js
@@ -0,0 +1,15 @@
+class homePageConfig 
+{
+    constructor()
+    {
+        this.title = "NTSJ Internal Services List";        // page title.
+        this.headerColor = "lightgray";                    // color of the page heading.
+        this.serviceWidth = "100%";                        // width of the services inside the center gray box.
+        this.serviceHeight = "65px";                       // height of each service.
+        this.serviceBGColor = "darkslateblue";             // background color of each service.
+        this.serviceLinkColor = "white";                   // text color of service links.
+        this.bgColor = "black";                            // page bg color - only if no image is used.
+        this.bgImage = "./images/dark-texture-bkg.jpg";    // page background image.
+    }
+}
+
