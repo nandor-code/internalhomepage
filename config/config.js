dfb93d5fe55533f3bb397f16e957b81da9714425
Zoltaroth <nandor@ntsj.com>
2019-08-15 07:14:23 -0700

    added netdata dashboard support - this project is no longer super generic.

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ class homePageConfig
         this.advDefault = true;                            // show advanced by default?
         this.bgColor = "black";                            // page bg color - only if no image is used.
         this.bgImage = "./images/dark-texture-bkg.jpg";    // page background image.
-        this.enableSonosState = false;                     // enable Sonos state checking
+        this.enableSonosState = true;                      // enable Sonos state checking
         this.servicesJson = "config/services.json";        // location of services json file.
     }
 }
